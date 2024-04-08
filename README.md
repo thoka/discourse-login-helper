265aa56a0114b4af64a0252d6f1091351eeb4acc
Thomas Kalka <thomas.kalka@gmail.com>
2024-04-08 09:11:19 +0200

    rename settings. add readme.  remove debug output

---
+++ README.md
@@ -0,0 +1,15 @@
+This discourse plugin is an attempt to simplify the login process for novice, mass subscribed users.
+
+Links to the forum (initially only links in the footer) are enriched with the user's e-mail address. 
+
+When the user follows a link to the usually restricted page, he is redirected to a pre-filled user registration form.
+
+After entering a password, users will be redirected to the restricted page.
+
+Next steps:
+
+- [ ] implement redirection to the page also for login via mail
+- [ ] enrich all links in notifications to discourse with user information
+
+
+ 
--- plugin.rb
+++ plugin.rb
@@ -1,12 +1,12 @@
 # name: discourse-login-helper
 # about: shorten process of loging in by email
-# version: 0.0.0
+# version: 0.0.1
 # authors: Thomas Kalka
 # url: https://github.com/thoka/discourse-login-helper
 # frozen_string_literal: true
 
 after_initialize do
-  # enabled_site_setting :login_helper_enabled
+  enabled_site_setting :loginhelper_enabled
 
   # p "⚠ discourse-login-helper running ..."
   # if username is provided in url, we redirect to login page directly providing the username in cookies
@@ -16,21 +16,24 @@ after_initialize do
     module BuildEmailHelperExtension
       def build_email(to, opts)
         opts ||= {}
-        puts "🔵 build_email opts=#{opts}"
+        # puts "🔵 build_email opts=#{opts}"
         opts[:url] += "?" + URI.encode_www_form({ login: to }).to_s if opts.key?(:url)
+
         super(to, opts)
       end
     end
 
-    # if username is provided in url, we redirect to login page directly
+    # if username is provided in url, we redirect to login page directly on failed topic access
     module ApplicationControllerExtension
       def rescue_discourse_actions(type, status_code, opts = nil)
         path = request.env["PATH_INFO"]
-        return super(type, status_code, opts) unless path.start_with? "/t/"
 
-        puts "🔵 rescue_discourse_actions type=#{type} opts=#{opts}"
-        puts "🔵 PATH_INFO=#{request.env["PATH_INFO"]}"
-        puts "🔵 params #{params}"
+        return super(type, status_code, opts) if type != :invalid_access || current_user.present?
+
+        # puts "🔵 rescue_discourse_actions type=#{type} opts=#{opts}"
+        # puts "🔵 PATH_INFO=#{request.env["PATH_INFO"]}"
+        # puts "🔵 params #{params}"
+        # puts "🔵 current_user #{current_user} present: #{current_user.present?}"
 
         cookies[:email] = params[:login] if params[:login].present?
         cookies[:email] ||= params[:email] if params[:email].present?
